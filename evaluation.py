024a697eb4ce632059d10312c47b8ff169356c4b
alli <allibusa@protonmail.com>
2022-11-28 12:34:36 +0100

    adding quote from paper, plotting MCC function

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 import random
+import matplotlib.pyplot as plt
 
 
 def MCC(true_z, predicted_z):
@@ -8,6 +9,12 @@ def MCC(true_z, predicted_z):
     and recovered latent variables for one environment 
 
     Uses Pearsons Corr Coef
+
+    from paper: 
+    We also compute the mean correlation coefficient (MCC) used in Khemakhem et al. (2020a), which
+    can be obtained by calculating the correlation coefficient between all pairs of true and recovered
+    latent factors and then solving a linear sum assignment problem by assigning each recovered latent
+    factor to the true latent factor with which it best correlates
 
     Args:
         true_z (numpy array): 2D dimensional numpy array, where columns represent variables
@@ -27,7 +34,18 @@ def MCC(true_z, predicted_z):
     return mcc
 
 
-# test
+def plot_MCC(mcc_model, mcc_mean, mcc_var):
+    """_summary_
+
+    Args:
+        mcc_model (list): names of models that MCC was performed on
+        mcc_mean (list): the returned value of the MCC function
+        mcc_var (list): the variance that corresponds with the mean values given above
+    """
+    plt.bar(mcc_model, mcc_mean, yerr=mcc_var)
+
+
+# test MCC
 random.seed(10)
 true_z = np.random.rand(4, 3)
 test_z = np.random.rand(4, 3)
